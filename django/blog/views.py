2760833ef7c147f663c3220b99b7862995ad1daa
jeongyeob <sjy20000@gmail.com>
2018-01-30 11:41:20 +0900

    blog.url 및 blog.views 수정

--- django/blog/views.py
+++ django/blog/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def post_list(request):
+    response = '<html><body><h1>Post List</h1><p>post 목록을 보여줄 예정입니다.</p></body></html>'
+    return HttpResponse(response)
+
+
+def post_list2(request):
+    return HttpResponse('post_list2')
