95eecab371f8a0419e2d4ab72f86080ebb8548a6
jeongyeob <sjy20000@gmail.com>
2018-01-30 14:28:55 +0900

    post list 출력

--- django/blog/views.py
+++ django/blog/views.py
@@ -1,13 +1,25 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Post
 
 
 def post_list(request):
-    return render(request, 'blog/post_list.html')
+    posts = Post.objects.all()
+    context = {
+        'posts': posts,
+    }
+    html = '''
+    '''
+    return render(
+        request=request,
+        template_name='blog/post_list.html',
+        context=context
+    )
+    # return render(request, 'blog/post_list.html', context)
 
 
 def post_list2(request):
-    return render(request, 'blog/post_list2.html')
+    return render(request, 'blog/notfound.html')
 
 
 def post_detail(requst):
--- django/config/settings.py
+++ django/config/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-TEMPLATES_DIR = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))),    'templates')
+TEMPLATES_DIR = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'templates')
 
 
 # Quick-start development settings - unsuitable for production
