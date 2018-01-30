2ecc2757e34dc47896756224f6b0671743716d99
jeongyeob <sjy20000@gmail.com>
2018-01-30 11:59:58 +0900

    blog template 추가

--- django/blog/views.py
+++ django/blog/views.py
@@ -3,8 +3,7 @@ from django.shortcuts import render
 
 
 def post_list(request):
-    response = '<html><body><h1>Post List</h1><p>post 목록을 보여줄 예정입니다.</p></body></html>'
-    return HttpResponse(response)
+    return render(request, 'blog/post_list.html')
 
 
 def post_list2(request):
